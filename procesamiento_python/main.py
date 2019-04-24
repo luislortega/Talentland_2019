7fcf8430e848a3265854b3dbe82131d11af25ee6
Luis Leon <leon.luisgerardo775@gmail.com>
2019-04-24 15:59:00 -0500

    Variables a tomar en cuenta

---
+++ procesamiento_python/main.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    #Contenedores de informacion
+    poblacion_2010 = []
+    natalidad_2010_2017 = []
+    mortalidad_2010_2017 = []
+    poblacion_2018_2019 = []
+    patentes_2010_2018 = []
+    unidades_economicas_2013_2018 = []
+    turistas_2010_2018 = []
+    pib_mexico_1993_2018 = []
+    pib_entidades_2010_2017 = []
+    promedio_actividad_trimestral_2010_2017 = [] #PROMEDIO
+    exportaciones_entidades_2010_2018 = []
