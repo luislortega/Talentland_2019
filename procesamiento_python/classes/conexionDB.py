015a24e8e35b196cf4314dd24f09ec8774bcf6f5
Luis Leon <leon.luisgerardo775@gmail.com>
2019-04-24 21:20:31 -0500

    Conexiones y controladores

---
+++ procesamiento_python/classes/conexionDB.py
@@ -0,0 +1,21 @@
+'''
+@author: Monkey Coders
+@version: 1
+
+Este prototipo en Python con estandar MVC, filtra y procesa los datos para poder ser exportado a otras plataformas.
+
+Condiciones:
+2010 - Actualidad
+'''
+import psycopg2
+import json
+
+class ConexionDB:
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect("dbname='grupomodelo' user='postgres' host='localhost' password='1298Luis'")
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+            print("[✔] Base de datos conectada")
+        except:
+            print("[x] Error en la conexion")
---
+++ procesamiento_python/classes/controlador.py
@@ -0,0 +1,10 @@
+'''
+@author: Monkey Coders
+@version: 1
+
+Este prototipo en Python con estandar MVC, filtra y procesa los datos para poder ser exportado a otras plataformas.
+
+Condiciones:
+2010 - Actualidad
+'''
+class ControladorDatos:
