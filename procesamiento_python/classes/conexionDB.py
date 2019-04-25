b8ed2ce7fca940e985d6321ecd9a4769a5c4cafb
Luis Leon <leon.luisgerardo775@gmail.com>
2019-04-25 03:48:10 -0500

    Mineria completa

--- procesamiento_python/classes/conexionDB.py
+++ procesamiento_python/classes/conexionDB.py
@@ -25,13 +25,89 @@ class ConexionDB:
         self.cursor.execute(create_table_command)
         create_table_command = "CREATE TABLE entidad_federativa(id serial PRIMARY KEY, nombre_entidad varchar(100), lat varchar, long varchar, exportaciones JSON, poblacion JSON, patentes JSON, unidades_economicas JSON, turismo JSON, actividad_economica_promedio JSON, actividades_economicas JSON)"
         self.cursor.execute(create_table_command)
-        create_table_command = "CREATE TABLE municipios(id serial PRIMARY KEY, consumo_cfe JSON, promedio_total_estatal varchar)"
+        create_table_command = "CREATE TABLE municipios(id serial PRIMARY KEY, lat varchar, long varchar, nombre varchar, consumo_cfe JSON, promedio_total_estatal varchar)"
         self.cursor.execute(create_table_command)
         print("[✔] Tablas de la bse de datos creadas")
 
     def limpiar_tablas_postgres(self):
-        drop_table_command = "DROP TABLE entidad_federativa"
-        self.cursor.execute(drop_table_command)
         drop_table_command = "DROP TABLE mexico"
         self.cursor.execute(drop_table_command)
+        drop_table_command = "DROP TABLE entidad_federativa"
+        self.cursor.execute(drop_table_command)
+        drop_table_command = "DROP TABLE municipios"
+        self.cursor.execute(drop_table_command)
+        
         print("[✔] Limpieza en las tablas en la base de datos")
+
+    def insertar_entidades_poblacion_2010(self, entidades, poblacion):
+        for i, elemento in enumerate(entidades):
+            insert_command = "INSERT INTO entidad_federativa(nombre_entidad, poblacion) VALUES('"+elemento+"', '"+json.dumps(poblacion[i])+"')"
+            self.cursor.execute(insert_command)
+        print("[✔] Indetidades federativas insertados en la base de datos con su poblacion en 2010")
+    
+    def insertar_poblacion_2010_2017(self, poblacion):
+        for x in range(1,33):
+            update_command = "UPDATE entidad_federativa SET poblacion='"+json.dumps(poblacion[x-1])+"' WHERE id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Poblacion hasta el 2017 insertados en la base de datos")
+    
+    def insertar_poblacion_2018_2019(self, poblacion):
+        self.cursor.execute("SELECT * from entidad_federativa")
+        entidades_federativas = self.cursor.fetchall()
+        for x in range(1, 33):
+            contenacion_datos = {**entidades_federativas[x-1][5], **poblacion[x-1]}
+            update_command = "UPDATE entidad_federativa SET poblacion='"+json.dumps(contenacion_datos)+"' WHERE id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Poblacion del 2018 y 2019 insertados en la base de datos")  
+
+    def insertar_patentes_2010_2018(self, patentes):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET patentes='"+json.dumps(patentes[x-1])+"' WHERE id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Patentes 2010 hasta el 2018 insertadas en la base de datos")
+    
+    def insertar_unidades_economicas_2013_2018(self, unidades_economicas_2013_2018):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET unidades_economicas='"+json.dumps(unidades_economicas_2013_2018[x-1])+"' where id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Unidades economicas del 2013 hasta el 2018 insertadas en la base de datos")
+    
+    def insertar_turistas_2010_2018(self, turistas_2010_2018):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET turismo='"+json.dumps(turistas_2010_2018[x-1])+"' where id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Turistas por entidad federativa del 2010 hasta el 2018 insertadas en la base de datos")
+
+    def insertar_pib_mexico_2010_2018(self, pib_mexico_2010_2018):
+        insert_command = "INSERT INTO mexico(pib) values('"+json.dumps(pib_mexico_2010_2018)+"')"
+        self.cursor.execute(insert_command)
+        print("[✔] Pib total de Mexico por año del 2010 hasta el 2018 insertado en la base de datos")
+
+    def insertar_poblacion_mexico_2010_2018(self, poblacion_mexico_2010_2018):
+        update_command = "UPDATE mexico SET poblacion_total='"+json.dumps(poblacion_mexico_2010_2018)+"'"
+        self.cursor.execute(update_command)
+        print("[✔] Poblacion total de Mexico por año del 2010 hasta el 2018 insertado en la base de datos")
+    
+    def insertar_exportaciones_entidades_2010_2018(self, exportaciones_entidades_2010_2018):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET exportaciones='"+json.dumps(exportaciones_entidades_2010_2018[x-1])+"' where id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Exportaciones totales por entidad federativa de 2010 hasta el 2018 insertado en la base de datos")
+
+    def insertar_promedio_actividad_trimestral_2010_2017(self,promedio_actividad_trimestral_2010_2017):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET actividad_economica_promedio='"+json.dumps(promedio_actividad_trimestral_2010_2017[x-1])+"' where id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Promedio de la actividad economica trimestral por año de 2010 hasta el 2018 insertado en la base de datos")
+
+    def insertar_actividades_economicas_entidades_2010_2017(self,actividades_economicas_entidades_2010_2017):
+        for x in range(1, 33):
+            update_command = "UPDATE entidad_federativa SET actividades_economicas='"+json.dumps(actividades_economicas_entidades_2010_2017[x-1])+"' where id="+str(x)
+            self.cursor.execute(update_command)
+        print("[✔] Promedio de la actividad economica trimestral por año de 2010 hasta el 2018 insertado en la base de datos")
+
+    def insertar_consumo_electrico_municipios_2010_2017(self, nombre_municipios, consumo_municipio):
+        for i, nombre in enumerate(nombre_municipios):
+            insert_command = "INSERT INTO municipios(nombre, consumo_cfe) VALUES ('"+str(nombre)+"','"+json.dumps(consumo_municipio[i])+"')"
+            self.cursor.execute(insert_command)
+        print("[✔] Consumo electrico por municipio en todo Mexico insertado en la base de datos")
--- procesamiento_python/main.py
+++ procesamiento_python/main.py
@@ -21,6 +21,109 @@ if __name__ == "__main__":
     unidades_economicas_2013_2018 = []
     turistas_2010_2018 = []
     pib_mexico_1993_2018 = []
-    pib_entidades_2010_2017 = []
+    actividades_economicas_entidades_2010_2017 = []
     promedio_actividad_trimestral_2010_2017 = [] #PROMEDIO
     exportaciones_entidades_2010_2018 = []
+    consumo_electrico_2010_2017 = []
+
+    #Utilidades
+    scanner = CsvScanner()
+    database = ConexionDB()
+    controlador = ControladorDatos()
+
+    #Base de datos
+    database.limpiar_tablas_postgres()
+    database.crear_tablas_postgres()
+
+    '''
+        Poblacion 2010.
+        Datos: @inegi
+    '''
+    poblacion_2010 = scanner.leer_poblacion_2010('inegi_data/pob_entidades/Poblacion_01.csv')
+    '''
+        Natalidad 2011 - 2017.
+        Datos: @inegi
+    '''
+    natalidad_2010_2017 = scanner.leer_natalidad_2010_2017('inegi_data/pob_entidades/Natalidad_01.csv')
+    '''
+        Moratlidad 2011 - 2017.
+        Datos: @inegi
+    '''
+    mortalidad_2010_2017 = scanner.leer_mortalidad_2010_2017('inegi_data/pob_entidades/Mortalidad_01.csv')
+    '''
+        Proyecciones de poblacion 2018.
+        Datos: @conapo
+    '''
+    poblacion_2018_2019 = scanner.leer_poblacion_2018_2019('conapo_data/pob_ini_proyecciones.csv')
+    '''
+        Patentes registradas por entidad federativa 2015 - 2018
+        Datos: @impi
+    '''
+    patentes_2010_2018 = scanner.leer_patentes_2010_2018('impi_data/patentes.csv')
+    '''
+        Unidades economicas registradas en cada entidad federativa 2013 - 2018
+        Datos: @denue
+    '''
+    unidades_economicas_2013_2018 = scanner.leer_unidades_economicas_2013_2018('inegi_data/neg_entidades/negocios.csv')
+    '''
+        Turistas registrados en cada entidad federativa 2010 - 2018
+        Datos: @sectur
+    '''
+    turistas_2010_2018 = scanner.leer_turistas_2010_2018('sectur_data/turismo_entidades.csv')
+    '''
+        PIB de Mexico 2010 - 2018
+        Datos: @inegi
+    '''
+    pib_mexico_1993_2018 = scanner.leer_pib_mexico_1993_2018('inegi_data/pib_mexico/pib_mexico.csv')
+    '''
+        PIB por entidad federativa y actividad economica 2010 - 2017
+        Datos: @inegi
+    '''
+    actividades_economicas_entidades_2010_2017 = scanner.leer_actividades_economicas_entidades_2010_2017(
+        ['inegi_data/pib_entidades/pibe_entidad_ags.csv', 'inegi_data/pib_entidades/pibe_entidad_bc.csv',
+         'inegi_data/pib_entidades/pibe_entidad_bcs.csv', 'inegi_data/pib_entidades/pibe_entidad_camp.csv',
+         'inegi_data/pib_entidades/pibe_entidad_coah.csv', 'inegi_data/pib_entidades/pibe_entidad_col.csv',
+         'inegi_data/pib_entidades/pibe_entidad_chis.csv',  'inegi_data/pib_entidades/pibe_entidad_chih.csv',
+         'inegi_data/pib_entidades/pibe_entidad_cdmx.csv', 'inegi_data/pib_entidades/pibe_entidad_dgo.csv',
+         'inegi_data/pib_entidades/pibe_entidad_gto.csv', 'inegi_data/pib_entidades/pibe_entidad_gro.csv',
+         'inegi_data/pib_entidades/pibe_entidad_hgo.csv', 'inegi_data/pib_entidades/pibe_entidad_jal.csv',
+         'inegi_data/pib_entidades/pibe_entidad_mex.csv', 'inegi_data/pib_entidades/pibe_entidad_mich.csv',
+         'inegi_data/pib_entidades/pibe_entidad_mor.csv', 'inegi_data/pib_entidades/pibe_entidad_nay.csv',
+         'inegi_data/pib_entidades/pibe_entidad_nl.csv','inegi_data/pib_entidades/pibe_entidad_oax.csv', 
+         'inegi_data/pib_entidades/pibe_entidad_pue.csv', 'inegi_data/pib_entidades/pibe_entidad_qr.csv', 
+         'inegi_data/pib_entidades/pibe_entidad_qro.csv','inegi_data/pib_entidades/pibe_entidad_slp.csv', 
+         'inegi_data/pib_entidades/pibe_entidad_sin.csv','inegi_data/pib_entidades/pibe_entidad_son.csv', 
+         'inegi_data/pib_entidades/pibe_entidad_tab.csv','inegi_data/pib_entidades/pibe_entidad_tamps.csv', 
+         'inegi_data/pib_entidades/pibe_entidad_tlax.csv', 'inegi_data/pib_entidades/pibe_entidad_ver.csv',
+          'inegi_data/pib_entidades/pibe_entidad_yuc.csv','inegi_data/pib_entidades/pibe_entidad_zac.csv']
+          )
+    '''
+        Indicador Trimestral de Actividad Económica Estatal 2010 - 2017
+        Datos: @ITAEE
+    '''
+    promedio_actividad_trimestral_2010_2017 = scanner.leer_promedio_actividad_trimestral_2010_2017('itaee_data/itaee_indice.csv')
+    '''
+        Exportaciones de mexico por entidad federativa 2010 - 2018 
+        Datos: @ADUANA
+    '''
+    exportaciones_entidades_2010_2018 = scanner.leer_exportaciones_entidades_2010_2018('inegi_data/exp_entidades/exportaciones.csv')
+    '''
+        Gastos en consumo de electricidad por municipio 2010 - 2017
+        Datos: @CFE
+    '''
+    consumo_electrico_2010_2017 = scanner.leer_consumo_electrico_municipios_2010_2017('cfe_data/consumo.csv')
+
+    #controladores
+    controlador.controlador_poblacion_2010(database, poblacion_2010)
+    controlador.controlador_poblacion_2010_2017(database, poblacion_2010, natalidad_2010_2017, mortalidad_2010_2017)
+    controlador.controlador_poblacion_2018_2019(database, poblacion_2018_2019)
+    controlador.controlador_patentes_2010_2018(database, patentes_2010_2018)
+    controlador.controlador_unidades_economicas_2013_2018(database, unidades_economicas_2013_2018)
+    controlador.controlador_turistas_2010_2018(database, turistas_2010_2018)
+    controlador.controlador_pib_mexico_1993_2018(database, pib_mexico_1993_2018)
+    controlador.controlador_poblacion_mexico_2010_2019(database, poblacion_2010, natalidad_2010_2017, mortalidad_2010_2017, poblacion_2018_2019)
+    controlador.controlador_actividades_economicas_entidades_2010_2017(database, actividades_economicas_entidades_2010_2017)
+    controlador.controlador_promedio_actividad_trimestral_2010_2017(database, promedio_actividad_trimestral_2010_2017)
+    controlador.controlador_exportaciones_entidades_2010_2018(database, exportaciones_entidades_2010_2018)
+    controlador.controlador_consumo_electrico_municipios_2010_2017(database, consumo_electrico_2010_2017)
+  
